136dee8572d08bb9bf30d753349f84f026adae75
Brian Louis <brianlouis.salinas116@gmail.com>
2025-11-06 14:12:01 -0700

    FINALLY WORKS HAHA

--- MarketSight/MarketSightBack/MSOAI.py
+++ MarketSight/MarketSightBack/MSOAI.py
@@ -65,7 +65,7 @@ def check_stock(stock: str) -> str:
 # MUST BE A STRING
 def StockInfo(symbol: str) -> str:
     # First check stock
-    symbol = symbol.capitalize
+    symbol = symbol.upper()
     stock = Ticker([symbol])
     financial_data = stock.financial_data.get(symbol, {})
     return financial_data
@@ -75,15 +75,17 @@ def StockSummary(stock: str) -> str:
     stock = stock.upper()
     # Check if stock exist,  if it deos pass response and return it
     if check_stock(stock):
+        information_of_stock = StockInfo(stock)
         response = client.messages.create(
             model=MODEL_AI,
             max_tokens=int(MAX_TOKENS),
             messages=[
-                {"role": "user", "content": f"Summarize  stock information and the updates it should have such as that it has"}
+                {"role": "user", 
+                 "content": f"Summarize  stock information {information_of_stock} and the updates it should have such as that it has it properly formmated such as that we're able to have it organized prompt, and we want it to be formmated and useable in Django (NO CODE, BUT THIS IS  A REFERENCE TO JUST FORMAT IT HAHA. DO NOT ADD THE REMAINING FORMALITY, JUST ADD INFORMATION ONLY!). What does the stock infomration entail the company and what should investors do when investing in {stock} "}
             ],
         )
             # Create a max tokens for users, MAKE SURE TO ADD THE MAX TOKEN VALUE IN ENV
-        return response
+        return response.content[0].text.strip()
     else:
         return f"The Stock {stock} Does not Exist, Please Try Again!"
 
@@ -93,4 +95,4 @@ def StockSummary(stock: str) -> str:
 
 
 
-print(StockSummary('AAPL'))
+print(type(StockSummary('AAPL')))
