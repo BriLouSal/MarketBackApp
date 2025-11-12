11eb050ec31e1dfa5e47133aca1071eadada7f0d
Brian Louis <brianlouis.salinas116@gmail.com>
2025-11-11 22:18:26 -0700

    Pass

---
+++ MarketSight/MarketSightBack/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+
+from django.utils import timezone
+
+
+import yfinance as yf
+
+import time
+
+
+MAX_LENGTH_OF_TITLE = 255
+
+# This should be the most important part of determining relevancy
+# as it should carry more weigh
+ACTIVITY_PARAMETERS = 1.25
+
+DURATION_DECAY = 0.5
+
+DEFAULT_VOTE = 0
+class Profile(models.Model):
+    username = models.CharField(max_length=100, null=True)
+    email = models.EmailField(null=True)
+    password = models.CharField(max_length=100)
+    created = models.DateField(auto_now_add=True)
+    money_owned = models.IntegerField(default=100000) # We want to have money for the Users to simulate stock trading. We'll do it in Model to remain dynamic and not remain static in views.
+
+    def __str__(self):
+        return self.username
+    
+
+class Chat(models.Model):
+    title = models.CharField(max_length=MAX_LENGTH_OF_TITLE)
+    
+    body = models.TextField()
+
+
+    
+    
+    created_at = models.DateField(default=timezone.now)
+    
+    upvotes = models.IntegerField(default=DEFAULT_VOTE)
+   
+    downvotes = models.IntegerField(default=DEFAULT_VOTE)
+   
+    replies_count = models.IntegerField(default=DEFAULT_VOTE)
+    # We need to convert self_created_at into integer in order to make my algorithim to work
+
+
+    # This algorithim will ensure the relevancy is accurate
+    def relevancy_algo(self):
+        time_now = timezone.now
+        time_decay = (time_now - self.created_at).total_second() / 86400
+        return (self.upvotes - self.downvotes) + (self.replies_count * ACTIVITY_PARAMETERS) + (time_decay * DURATION_DECAY)
+
+
+
+        
+
+# User's owner of that Portfolio
+# Requirements: Name and the Ticker of the Stock. The Book Cost, and the Total Return of that stock. 
+
+#  In order to find the 
+class Portfolio(models.Model):
+    name = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=10)
+
+    
+    def __str__(self):
+        return f"{self.name} ({self.ticker})"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,34 @@
+
+## Roadmap
+
+
+Roadmap
+
+- Adding Community feature
+    - 
+    In progress 
+     -
+      Search Engine: Yahoo Query Calls.
+      -
+  
+      CLAUDE AI: USING CLAUDE AI FOR ITS POWERFUL USAGE, AND SPECIFIC BENEFITS THAT ALLOWS ME TO CREATE AMAZING PROMPTS!, Creating a News Positive Analysis via Sci-Kit Learn to train the model....
+        -
+      Investment Strategies ~ Shareable with Communities to follow
+            Adding Reputation System by using Upvotes system, and Downvote system
+    
+
+- Add more Stock Features
+    -
+    ALPACA API
+    -
+    MatPlot.Lib Library (Considering Using JS for this endeavour)
+     -
+    Investment Strategies (Using API Calls from ChatGPT to create analysis, and also using positivity and negativity algorithims to determine the company's recent media stunts). I will learn about more about
+    methods to determine if the stock's company has a great outlook based on ESG, or current outlook from the news media, and social media.
+  
+    
+
+-  Finished Products
+    - 
+    User_Authentication, Stock Financial Report Summary, Search Engine
+
