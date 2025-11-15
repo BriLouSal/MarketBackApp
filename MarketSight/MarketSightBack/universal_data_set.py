039384807e1b6a309605966603b68e0b13bbfe9f
Brian Louis <brianlouis.salinas116@gmail.com>
2025-11-14 19:17:19 -0700

    Change Data } Ensures around 90% Accuracy for Financal News Analysis

--- MarketSight/MarketSightBack/universal_data_set.py
+++ MarketSight/MarketSightBack/universal_data_set.py
@@ -32,3 +32,16 @@ load_dotenv()
 
 
 
+def generate_universal_dataset():
+    """Generate one dataset for all stocks using fictional company data."""
+    dataset = {
+        "Positive": info_to_positivty_rating_positivety(),
+        "Neutral":  info_to_positivty_rating_netural(),
+        "Negative": info_to_positivty_rating_negative()
+    }
+
+    with open('sentiment_dataset_of_stocks.json', "w", encoding="utf-8") as f:
+        json.dump(dataset, f, indent=4)
+
+
+generate_universal_dataset()
